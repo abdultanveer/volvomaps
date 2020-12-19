445fbbcafc3dac7032ec9cb2300b68798546d447
abdul <abdul.tanveer@gmail.com>
2020-12-19 12:03:36 +0530

    location part -1

--- app/src/main/java/com/next/volvomaps/MainActivity.java
+++ app/src/main/java/com/next/volvomaps/MainActivity.java
@@ -2,13 +2,48 @@ package com.next.volvomaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+    /**
+     * Provides the entry point to the Fused Location Provider API.
+     */
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
+
+    private String mLatitudeLabel;
+    private String mLongitudeLabel;
+    private TextView mLatitudeText;
+    private TextView mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLatitudeLabel = getResources().getString(R.string.latitude_label);
+        mLongitudeLabel = getResources().getString(R.string.longitude_label);
+        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
+        mLongitudeText = (TextView) findViewById((R.id.longitude_text));
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);  //connecting to my google play
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
